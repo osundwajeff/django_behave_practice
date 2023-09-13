f9c59eeb3c2aa8c5e3fa1819e7b909bcf79f8124
Jeff Osundwa <mulamajeff@tuta.io>
2023-09-13 17:16:56 +0300

    minor updates

--- farmtables/farms/features/steps/test.py
+++ farmtables/farms/features/steps/test.py
@@ -8,32 +8,34 @@ use_step_matcher("re")
 
 # global variables
 firefox_options = Options()
-firefox_options.add_argument("--headless")
+#firefox_options.add_argument("--headless")
 
 
 @given("the user is on the homepage")
-def step_iml(context):
+def step_user_is_on_homepage(context):
     context.selenium = webdriver.Firefox(options=firefox_options)
     # homepage
     context.selenium.get(f"http://127.0.0.1:8000/")
 
 
 @then("page is homepage")
-def step_iml(context):
+def step_page_is_homepage(context):
     # Assert that the title matches the expected title
     expected_title = "Home"
     assert context.selenium.title == expected_title
+    # Close the browser window
+    context.selenium.quit()
 
 
 @given("user is on 'Admin Log in page'")
-def step_iml(context):
+def step_user_is_on_admin_login(context):
     context.selenium = webdriver.Firefox(options=firefox_options)
     # login to admin panel
     context.selenium.get(f"http://127.0.0.1:8000/admin/")
 
 
 @when("user clicks on 'Log in'")
-def step_iml(context):
+def step_clicks_on_login(context):
     # fill in login information
     username = context.selenium.find_element(By.ID, 'id_username')
     username.send_keys("jeff")
@@ -45,26 +47,30 @@ def step_iml(context):
 
 
 @then("page is 'Admin page'")
-def step_iml(context):
+def step_page_is_admin_page(context):
     # Assert that the title matches the expected title
     expected_title = "Site administration | Django site admin"
     assert context.selenium.title == expected_title
+    # Close the browser window
+    context.selenium.quit()
 
 
 @given("user is on the homepage")
-def step_iml(context):
+def step_user_is_on_homepage(context):
     context.selenium = webdriver.Firefox(options=firefox_options)
     # homepage
     context.selenium.get(f"http://127.0.0.1:8000/")
 
 
 @when("user clicks on 'Point of Interest' link")
-def step_iml(context):
+def step_clicks_on_poi(context):
     context.selenium.find_element(By.LINK_TEXT, 'Point of Interest').click()
 
 
 @then("the page is 'point_of_interest' page")
-def step_iml(context):
+def step_page_is_poi(context):
     # Assert that the title matches the expected title
     expected_title = "Point of Interests"
     assert context.selenium.title == expected_title
+    # Close the browser window
+    context.selenium.quit()
